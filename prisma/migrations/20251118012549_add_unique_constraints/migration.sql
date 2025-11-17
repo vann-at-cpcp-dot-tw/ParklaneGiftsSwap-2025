ec02adc8fe2b42155536a8d76de64d5a7e6d3c3b
vann-at-cpcp-dot-tw <vann@cpcp.tw>
2025-11-18 01:26:33 +0800

    Add migration for unique constraints on participantNumber and realParticipantNo

---
+++ prisma/migrations/20251118012549_add_unique_constraints/migration.sql
@@ -0,0 +1,5 @@
+-- CreateIndex
+CREATE UNIQUE INDEX "Submission_participantNumber_key" ON "Submission"("participantNumber");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Submission_realParticipantNo_key" ON "Submission"("realParticipantNo");
